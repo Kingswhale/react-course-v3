4ef8d6aa348c25b0ccb199edda03f813480bafed
kingswhale <hardeyreemyarfyc@gmail.com>
2024-06-05 07:32:54 +0100

    update

--- 03-advanced-react/src/tutorial/06-forms/starter/03-multiple-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/03-multiple-inputs.jsx
@@ -1,28 +1,67 @@
+import { useState } from 'react'
+
 const MultipleInputs = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+
+  const handleChange = (e) => {
+    // console.log('this is happening here', e.target.name)
+    // console.log('this is happening here', e.target.value)
+    setUser({ ...user, [e.target.name]: [e.target.value] })
+    console.log('this is happening here', user)
+  }
+  const clickSubmit = (e) => {
+    e.preventDefault()
+    console.log('can you se the new user details: ', user)
+  }
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={clickSubmit}>
         <h4>Multiple Inputs</h4>
         {/* name */}
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type='text'
+            className='form-input'
+            id='name'
+            value={user.name}
+            onChange={handleChange}
+            name='name'
+          />
         </div>
         {/* email */}
         <div className='form-row'>
           <label htmlFor='email' className='form-label'>
             Email
           </label>
-          <input type='email' className='form-input' id='email' />
+          <input
+            type='email'
+            className='form-input'
+            id='email'
+            value={user.email}
+            onChange={handleChange}
+            name='email'
+          />
         </div>
         {/* email */}
         <div className='form-row'>
           <label htmlFor='password' className='form-label'>
             Password
           </label>
-          <input type='password' className='form-input' id='password' />
+          <input
+            type='password'
+            className='form-input'
+            id='password'
+            value={user.password}
+            onChange={handleChange}
+            name='password'
+          />
         </div>
 
         <button type='submit' className='btn btn-block'>
@@ -30,6 +69,6 @@ const MultipleInputs = () => {
         </button>
       </form>
     </div>
-  );
-};
-export default MultipleInputs;
+  )
+}
+export default MultipleInputs
--- 03-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,6 +1,17 @@
-import { useState } from 'react';
-const frameworks = ['react', 'angular', 'vue', 'svelte'];
+import { useState } from 'react'
+const frameworks = ['react', 'angular', 'vue', 'svelte']
 const OtherInputs = () => {
+  const [shipping, setShipping] = useState(false)
+  const [framework, setFramework] = useState('react')
+
+  const handleChecked = (e) => {
+    console.log('here we are', e.target.checked)
+    setShipping(e.target.checked)
+  }
+  const handleSelect = (e) => {
+    setFramework(e.target.value)
+  }
+  console.log('Handle Select', framework)
   return (
     <div>
       <form className='form'>
@@ -8,17 +19,34 @@ const OtherInputs = () => {
         {/* name */}
         <div className='form-row' style={{ textAlign: 'left' }}>
           <label htmlFor='shipping'> Free Shipping </label>
+          <input
+            type='checkbox'
+            name='shipping'
+            id='shipping'
+            checked={shipping}
+            onChange={handleChecked}
+          />
         </div>
         <div className='form-row' style={{ textAlign: 'left' }}>
           <label htmlFor='framework' className='form-label'>
             Framework
           </label>
+          <select
+            name='framework'
+            id='framework'
+            value={framework}
+            onChange={handleSelect}
+          >
+            {frameworks.map((framework) => {
+              return <option key={framework}>{framework}</option>
+            })}
+          </select>
         </div>
         <button type='submit' className='btn btn-block'>
           submit
         </button>
       </form>
     </div>
-  );
-};
-export default OtherInputs;
+  )
+}
+export default OtherInputs
